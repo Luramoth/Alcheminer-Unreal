cbee31d34f84408fcce5576f91a47ab40369be7a
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-09-29 18:13:16 -0700

    make the map scripted

---
+++ Source/Alcheminer/mainmap.h
@@ -0,0 +1,17 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "mainmap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALCHEMINER_API Amainmap : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};
