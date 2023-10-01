e73f6aeb3055820ed177ce006b2cfa8962df6dcc
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-10-01 02:08:37 -0700

    make alcheminer game instance

---
+++ Source/Alcheminer/AlcheminerGame.h
@@ -0,0 +1,17 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AlcheminerGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALCHEMINER_API UAlcheminerGame : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
